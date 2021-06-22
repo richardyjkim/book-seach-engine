37aa8e0598dba5adfe5280adccaa60862474f756
Richard <richardyjkim@gmail.com>
2021-06-22 16:56:12 -0700

    graphql bug

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,4 +17,31 @@ type User {
   bookCount: Int
   savedBooks: [Book]
 }
-`
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+input bookInput {
+  bookId: String
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+
+type Query {
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!, email: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(input: bookInput): User
+  removeBook(bookId: String!): User
+}
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const { typeDefs, resolver } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
