95e81cf60ac0efd9c5186dc060177a6984353b0d
Richard <richardyjkim@gmail.com>
2021-06-25 15:29:05 -0700

    deployment error #3

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -20,7 +20,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+// app.use(routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
