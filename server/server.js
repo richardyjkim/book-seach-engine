a2e97761bcf4648224890d28ff4d61c0b639862d
Richard <richardyjkim@gmail.com>
2021-06-25 14:32:10 -0700

    optimization

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(routes);
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.statuc(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
